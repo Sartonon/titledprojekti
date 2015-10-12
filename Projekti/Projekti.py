af200a1f19eaae221e6bbf3c313562194d5a65cc
Santeri Rusila <sajukaru@student.jyu.fi>
2015-10-12 18:11:52 +0300

    Toimiiko PyCharmin kautta

--- Projekti/Projekti.py
+++ Projekti/Projekti.py
@@ -13,7 +13,7 @@ def hello_world():
     for component in cal.walk('vevent'):
         print (component.get('summary'))
         print (component.get('location'))
-    return "Hello Worldg!"
+    return "Hello World!"
 
 @app.route('/luku')
 def luku():
