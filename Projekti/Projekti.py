e3c45ed2536a70a611d8902b1fe1fc3721872c03
Santeri Rusila <sajukaru@student.jyu.fi>
2015-10-14 13:09:18 +0300

    kokeilu

--- Projekti/Projekti.py
+++ Projekti/Projekti.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from icalendar import Calendar
+from flask import render_template, flash, redirect
 import requests
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
@@ -15,9 +18,16 @@ def hello_world():
         print (component.get('location'))
     return "Hello Worldg!"
 
-@app.route('/luku')
-def luku():
-    return "Hello"
+@app.route('/indeksi', methods=['GET', 'POST'])
+def indeksi():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for URL="%s"' %
+              (form.url.data))
+        return redirect('/indeksi')
+    return render_template('indeksi.html',
+                           title='Sign In',
+                           form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
