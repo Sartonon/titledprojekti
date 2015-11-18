739b889401d5a88bbc9f1cc96d06560d88781e81
Toni Pitkänen <toni.pitkanen@jyu.fi>
2015-11-18 18:31:32 +0200

    halleluja, robotti käy nyt linkkejä läpi yks kerrallaan

--- Projekti/screippaus.py
+++ Projekti/screippaus.py
@@ -1,23 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def screippaa():
     browser = webdriver.Firefox()
     browser.get("https://korppi.jyu.fi/openid/manage/endpoint?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.claimed_id=https%3A%2F%2Fkorppi.jyu.fi%2Fopenid%2F&openid.identity=https%3A%2F%2Fkorppi.jyu.fi%2Fopenid%2F&openid.return_to=https%3A%2F%2Fkorppi.jyu.fi%2Fkotka%2Fservlet%2Fauthentication%2FconsumeOpenIdResult&openid.realm=https%3A%2F%2Fkorppi.jyu.fi%2Fkotka%2Fservlet%2Fauthentication%2FconsumeOpenIdResult&openid.assoc_handle=1439986517853-17587&openid.mode=checkid_setup&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fsreg%2F1.0&openid.sreg.optional=email%2Cfullname")
     browser.implicitly_wait(10)
-    # username = WebDriverWait(browser, 10).until(
-    #     EC.presence_of_element_located((By.ID, "username"))
-    # )
-    # password = browser.find_element_by_id("password")
-    # username.send_keys("username")
-    # password.send_keys("password")
-    # browser.find_element_by_id("loginbutton").click()
-    browser.get("https://korppi.jyu.fi/kotka/reservation/searchSpace.jsp?code=&submitSearch=Hae&officialOnly=true&mapOnly=true")
 
-    td_list = WebDriverWait(browser, 10).until(lambda driver: driver.find_element_by_id("spaceResultTable")) #TODO: klikkaa karttaa
-    for td in td_list:
-        if(td.text == "Kartta"):
-            td.click()
+    browser.get("https://korppi.jyu.fi/kotka/reservation/searchSpace.jsp?code=&submitSearch=Hae&officialOnly=true&mapOnly=true")
+    global linkit
+    linkit = browser.find_elements_by_link_text('Kartta')
+    for linkki in range (1, len(linkit)):
+        linkit = browser.find_elements_by_link_text('Kartta')
+        linkit[linkki].click()
+        browser.implicitly_wait(5)
+        browser.back()
+        browser.implicitly_wait(5)
+
 screippaa()
