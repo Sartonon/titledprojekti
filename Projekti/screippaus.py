03e48784cfd611fd4b895c13d0e20a95f9ce4f65
Markus Valkama <majovalk@student.jyu.fi>
2015-11-23 16:20:25 +0200

    Tein vain muokkauksen lista.py:yn. Ei valita listasta ni pitäis olla sen kunnossa. Jotain häikkää on, mutta vika voi olla missä vaan.

--- Projekti/screippaus.py
+++ Projekti/screippaus.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from selenium import webdriver
+import selenium.webdriver.support.ui as ui
 import simplejson
 
 def screippaa():
@@ -8,7 +9,9 @@ def screippaa():
     browser.get('https://korppi.jyu.fi/openid/manage/endpoint?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.claimed_id=https%3A%2F%2Fkorppi.jyu.fi%2Fopenid%2F&openid.identity=https%3A%2F%2Fkorppi.jyu.fi%2Fopenid%2F&openid.return_to=https%3A%2F%2Fkorppi.jyu.fi%2Fkotka%2Fservlet%2Fauthentication%2FconsumeOpenIdResult&openid.realm=https%3A%2F%2Fkorppi.jyu.fi%2Fkotka%2Fservlet%2Fauthentication%2FconsumeOpenIdResult&openid.assoc_handle=1439986517853-17587&openid.mode=checkid_setup&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fsreg%2F1.0&openid.sreg.optional=email%2Cfullname')
     browser.implicitly_wait(10)
 
-    browser.get('https://korppi.jyu.fi/kotka/reservation/searchSpace.jsp?code=&submitSearch=Hae&officialOnly=true&mapOnly=true')
+    browser.get('https://korppi.jyu.fi/kotka/reservation/searchSpace.jsp')
+    browser.implicitly_wait(100)
+
     global linkit
     linkit = browser.find_elements_by_link_text('Kartta') #browser.find_elements_by_link_text('Rakennus') or browser.find_elements_by_link_text('Kartta')  #((By.id("foo"),By.name("bar")));# browser.find_elements_by_link_text('Kartta')
     for linkki in range(1, len(linkit)):
