e0191c357de887a46dc21adb0f061ab8b44ec9ca
Toni Pitkänen <toni.pitkanen@jyu.fi>
2016-01-14 21:47:59 +0200

    copyright-jutut lisätty ja muutama pilkunviilaus

--- Projekti/lista.py
+++ Projekti/lista.py
@@ -1,3 +1,21 @@
+# Copyright 2016  Jose Malmberg, Toni Pitkänen, Santeri Rusila, Markus Valkama
+#
+#
+# This file is part of Karttakotka.
+#
+# Karttakotka is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Karttakotka is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Karttakotka.  If not, see <http://www.gnu.org/licenses/>.
+
 def listaDict():
     listaDict = {"a 101": {"lon": 25.734484311607144, "lat": 62.237510144261485},
                  "a 102": {"lon": 25.734540105800715, "lat": 62.23753383250188},
--- Projekti/paikat.py
+++ Projekti/paikat.py
@@ -1,1 +1,19 @@
+# Copyright 2016  Jose Malmberg, Toni Pitkänen, Santeri Rusila, Markus Valkama
+#
+#
+# This file is part of Karttakotka.
+#
+# Karttakotka is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Karttakotka is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Karttakotka.  If not, see <http://www.gnu.org/licenses/>.
+
 {"Ylist%C3%B6nrinne Kirjasto": {"lon": 25.73981722387906, "lat": 62.22936473611648}, "Ylist%C3%B6nrinne O": {"lon": 25.740363432424193, "lat": 62.22904697548922}, "Ylist%C3%B6Kem3": {"lon": 25.739623996636464, "lat": 62.22923902245527}, "Ylist%C3%B6Kem2": {"lon": 25.73960206573708, "lat": 62.229222234295875}, "Ylist%C3%B6nrinne": {"lon": 25.74099227999004, "lat": 62.2291084166266}, "Ylist%C3%B6Kem1": {"lon": 25.739425505387313, "lat": 62.229276067610826}, "Ylist%C3%B6nrinne F": {"lon": 25.738998259197505, "lat": 62.22885663883185}, "Ylist%C3%B6nrinne E": {"lon": 25.739699167629713, "lat": 62.229027080615886}}
